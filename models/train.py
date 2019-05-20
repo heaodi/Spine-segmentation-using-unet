b5c1676e13c8a26a7f1b624091534e8bf776f6dc
heaodi <1416151860@qq.com>
2019-05-20 16:05:35 +0800

    second commit

--- models/train.py
+++ models/train.py
@@ -93,14 +93,14 @@ if __name__ == '__main__':
     data_gen_args = dict(samplewise_std_normalization=False, samplewise_center=True,  rotation_range=0, width_shift_range=0.05, height_shift_range=0.1,
                          shear_range=0.05, zoom_range=0.05, horizontal_flip=True, fill_mode='constant', cval=0)
 
-    trainGen = trainGenerator(1, train_path, 'image', 'label', data_gen_args, save_to_dir=False)
-    validGen = trainGenerator(1, valid_path, 'image', 'label', data_gen_args, save_to_dir=False)
+    trainGen = trainGenerator(4, train_path, 'image', 'label', data_gen_args, save_to_dir=False)
+    validGen = trainGenerator(4, valid_path, 'image', 'label', data_gen_args, save_to_dir=False)
     model = unet(input_size=(256, 256, 1))
     # model = unet(model_save_path + "unet_spine5.hdf5", input_size=(880, 880, 1))
     model_checkpoint = ModelCheckpoint(model_save_path+"unet_spine.hdf5", monitor='loss', verbose=2, save_best_only=True)
     history = LossHistory()
-    model.fit_generator(trainGen, steps_per_epoch=1104*2/8, validation_data=validGen, validation_steps=31,
-                        epochs=30, verbose=2, callbacks=[history])                   #validation_steps=126*2
+    model.fit_generator(trainGen, steps_per_epoch=1104*2/4, validation_data=validGen, validation_steps=62,
+                        epochs=30, verbose=1, callbacks=[history])                   #validation_steps=126*2
     history.loss_plot('epoch')
 
     # model = load_model(model_save_path + "2019-05-18_17-13_98.24.h5")
